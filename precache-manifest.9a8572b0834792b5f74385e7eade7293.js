b983d3fbfaf21d713558302887c64d312be4c574
cakas <cakas@naver.com>
2020-03-03 08:11:45 +0900

    Updates

---
+++ precache-manifest.9a8572b0834792b5f74385e7eade7293.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b6920ad3ba6a2335692dfa672f0b494",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "bbb0fa6f6a2fcca5414e",
+    "url": "/movie_app/static/css/main.d57d41c7.chunk.css"
+  },
+  {
+    "revision": "1a840292803cee922565",
+    "url": "/movie_app/static/js/2.9944a4a9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app/static/js/2.9944a4a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb0fa6f6a2fcca5414e",
+    "url": "/movie_app/static/js/main.6e3f0ec6.chunk.js"
+  },
+  {
+    "revision": "c0057b52e72b0f614c37",
+    "url": "/movie_app/static/js/runtime-main.15be7292.js"
+  }
+]);
